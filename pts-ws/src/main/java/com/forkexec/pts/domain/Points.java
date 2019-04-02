5b0809ee210aae0b788d58a26670477caa94ab02
RafaelPAndrade <rafael@localhost.here>
2019-04-02 18:40:27 +0100

    Add missing import in pts.domain

--- pts-ws/src/main/java/com/forkexec/pts/domain/Points.java
+++ pts-ws/src/main/java/com/forkexec/pts/domain/Points.java
@@ -1,5 +1,6 @@
 package com.forkexec.pts.domain;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Pattern;
