61ed4bf749d04cc1c92e273adec2da17f47a667e
LivioCosta <livio.cos@gmail.com>
2019-04-02 16:06:24 +0100

    Change comment

--- pts-ws-cli/src/test/java/com/forkexec/pts/ws/it/ActivateUserIT.java
+++ pts-ws-cli/src/test/java/com/forkexec/pts/ws/it/ActivateUserIT.java
@@ -7,7 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Class that tests Users operation
+ * Class that tests ActivateUser operation
  */
 public class ActivateUserIT extends BaseIT {
 	
