de376aceb68db84db20d603f87ecc114648f5cc8
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2019-04-03 14:12:10 -0400

    Finish implementing tests for searchMenus()

--- rst-ws-cli/src/test/java/com/forkexec/rst/ws/it/SearchMenusIT.java
+++ rst-ws-cli/src/test/java/com/forkexec/rst/ws/it/SearchMenusIT.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Test;
 import org.junit.Before;
 
+import com.forkexec.rst.ws.Menu;
 import com.forkexec.rst.ws.MenuInit;
 import com.forkexec.rst.ws.BadInitFault_Exception;
 import com.forkexec.rst.ws.BadTextFault_Exception;
@@ -12,6 +13,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 import static com.forkexec.rst.ws.it.ViewUtils.*;
+import static org.junit.Assert.assertEquals;
 
 
 public class SearchMenusIT extends BaseIT {
@@ -57,11 +59,67 @@ public class SearchMenusIT extends BaseIT {
         client.searchMenus(" pizza");
     }
 
-    /*@Test (expected = BadTextFault_Exception.class)
-    public void descriptionWithWeirdSymbols() throws BadTextFault_Exception {
-        client.searchMenus("Л"); 
-    } */
+    @Test (expected = BadTextFault_Exception.class)
+    public void descriptionWithSpaces4() throws BadTextFault_Exception {
+        client.searchMenus("gelado de chocolate");
+    }
 
+    @Test
+    public void descriptionNonExistant() throws BadTextFault_Exception {
+        List<Menu> menus = client.searchMenus("bomdia");
+        assertEquals(menus.size(), 0);
+    }
+
+    @Test 
+    public void success1() throws BadTextFault_Exception {
+        List<Menu> menus = client.searchMenus("picanha");
+        assertEquals(menus.size(), 1);
+        assertEquals(menus.get(0).getId().getId(), "10");
+        assertEquals(menus.get(0).getPrice(), 250);
+    }
+
+    @Test 
+    public void success2() throws BadTextFault_Exception {
+        List<Menu> menus = client.searchMenus("tremoço");
+        assertEquals(menus.size(), 1);
+        assertEquals(menus.get(0).getId().getId(), "4");
+        assertEquals(menus.get(0).getPlate(), "cachupa");
+    }
+
+    @Test 
+    public void success3() throws BadTextFault_Exception {
+        List<Menu> menus = client.searchMenus("gelado");
+        assertEquals(menus.size(), 2);
+    }
+
+    @Test 
+    public void success4() throws BadTextFault_Exception {
+        List<Menu> menus = client.searchMenus("azeitonas");
+        assertEquals(menus.size(), 2);
+    }
+
+    @Test 
+    public void success5() throws BadTextFault_Exception {
+        List<Menu> menus = client.searchMenus("pizza");
+        assertEquals(menus.size(), 2);
+    }
+
+    @Test 
+    public void success6() throws BadTextFault_Exception {
+        List<Menu> menus = client.searchMenus("tarte");
+        assertEquals(menus.size(), 2);
+    }
+
+    @Test 
+    public void success7() throws BadTextFault_Exception {
+        List<Menu> menus = client.searchMenus("pão");
+        assertEquals(menus.size(), 3);
+    }
+
+    @After
+    public void tearDown() {
+        client.ctrlClear();
+    }
 
 
 }
