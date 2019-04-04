54ed6ec4ddfe712af45301032890678f3037c462
LivioCosta <livio.cos@gmail.com>
2019-04-04 10:12:08 +0100

    Edit class to use static cc url

--- cc-ws-cli/src/main/java/pt/ulisboa/tecnico/sdis/ws/cc/CreditCardClient.java
+++ cc-ws-cli/src/main/java/pt/ulisboa/tecnico/sdis/ws/cc/CreditCardClient.java
@@ -26,76 +26,26 @@ public class CreditCardClient implements CreditCard {
 	/** WS port (port type is the interface, port is the implementation) */
 	CreditCard port = null;
 
-	/** UDDI server URL */
-	private String uddiURL = null;
-
-	/** WS name */
-	private String wsName = null;
-
 	/** WS end point address */
-	private String wsURL = null; // default value is defined inside WSDL
+	private static final String wsURL = "http://ws.sd.rnl.tecnico.ulisboa.pt:8080/cc";
 
 	public String getWsURL() {
 		return wsURL;
 	}
 
-	/** output option **/
-	private boolean verbose = false;
-
-	public boolean isVerbose() {
-		return verbose;
-	}
-
-	public void setVerbose(boolean verbose) {
-		this.verbose = verbose;
-	}
-
 	/** constructor with provided web service URL */
-	public CreditCardClient(String wsURL) throws CreditCardClientException {
-		this.wsURL = wsURL;
+	public CreditCardClient() throws CreditCardClientException {
 		createStub();
-	}
-
-	/** constructor with provided UDDI location and name */
-	public CreditCardClient(String uddiURL, String wsName) throws CreditCardClientException {
-		this.uddiURL = uddiURL;
-		this.wsName = wsName;
-		uddiLookup();
-		createStub();
-	}
-
-	/** UDDI lookup */
-	private void uddiLookup() throws CreditCardClientException {
-		try {
-			if (verbose)
-				System.out.printf("Contacting UDDI at %s%n", uddiURL);
-			UDDINaming uddiNaming = new UDDINaming(uddiURL);
-
-			if (verbose)
-				System.out.printf("Looking for '%s'%n", wsName);
-			wsURL = uddiNaming.lookup(wsName);
-
-		} catch (Exception e) {
-			String msg = String.format("Client failed lookup on UDDI at %s!", uddiURL);
-			throw new CreditCardClientException(msg, e);
-		}
-
-		if (wsURL == null) {
-			String msg = String.format("Service with name %s not found on UDDI at %s", wsName, uddiURL);
-			throw new CreditCardClientException(msg);
-		}
 	}
 
 	/** Stub creation and configuration */
 	private void createStub() {
-		if (verbose)
-			System.out.println("Creating stub ...");
+		System.out.println("Creating stub ...");
 		service = new CreditCardImplService();
 		port = service.getCreditCardImplPort();
 
 		if (wsURL != null) {
-			if (verbose)
-				System.out.println("Setting endpoint address ...");
+			System.out.println("Setting endpoint address ...");
 			BindingProvider bindingProvider = (BindingProvider) port;
 			Map<String, Object> requestContext = bindingProvider.getRequestContext();
 			requestContext.put(ENDPOINT_ADDRESS_PROPERTY, wsURL);
