873fc5d141a35d018f20d027920085c3a2a74ed9
RafaelPAndrade <rafael@localhost.here>
2019-05-03 09:10:04 +0100

    Add some demo cases
    Missing implementing concurrency demonstrations
    
    Changes to be committed:
    	modified:   pts-ws-cli/.../PointsClientApp.java

--- pts-ws-cli/src/main/java/com/forkexec/pts/ws/cli/PointsClientApp.java
+++ pts-ws-cli/src/main/java/com/forkexec/pts/ws/cli/PointsClientApp.java
@@ -22,7 +22,11 @@ public class PointsClientApp {
 		// Create frontend
 		PointsFrontEnd frontEnd = new PointsFrontEnd(n, uddiURL);
 
+		// To help measure time taken
+		long start;
 
+		frontEnd.ctrlClear();
+		System.out.printf("Starting test case %s\n", testCase);
 		switch(testCase){
 			case "ping":
 				System.out.println("Invoke ping()...");
@@ -30,17 +34,34 @@ public class PointsClientApp {
 				System.out.print("Result: ");
 				System.out.println(result);
 			break;
-			case "F1":
-				System.out.println("Starting test case F1");
-				frontEnd.ctrlFail(1, "delay:5");
+			case "F1": // normal case
 				frontEnd.pointsBalance("a@b");
-				System.out.println("Ending test case F1");
 			break;
-			case "F2":
-				System.out.println("Starting end case F2");
-				System.out.println("Ending test case F2");
+			case "F2": // fault demonstration (delay)
+				frontEnd.ctrlFail(1, "delay:10");
+				frontEnd.ctrlFail(2, "delay:5");
+				frontEnd.pointsBalance("a@b");
+			break;
+			case "F3": // Cache demonstration (only 1 read, the rest is cache based)
+				start = System.currentTimeMillis();
+				for(int i = 0; i < 1000; ++i) { frontEnd.pointsBalance("a@b"); }
+				System.out.printf("Total time taken: %d ms\n", System.currentTimeMillis() - start);
+			break;
+			case "F4": // Comparision for implementation without caching
+				start = System.currentTimeMillis();
+				for(int i = 0; i < 10; ++i) { (new PointsFrontEnd(n, uddiURL)).pointsBalance("a@b"); }
+				System.out.printf("Total time taken: %d ms\n", System.currentTimeMillis() - start);
+			break;
+			case "F5": // Concurrent access: reads on same address
+			break;
+			case "F6": // Concurrent access: writes on same address
+			break;
+			case "F7": // Concurrent access: reads on diferent addresses
+			break;
+			case "F8": // Concurrent access: writes on diferent addresses
 			break;
 		}
+		System.out.printf("Ending test case %s\n", testCase);
 	}
 
 }
